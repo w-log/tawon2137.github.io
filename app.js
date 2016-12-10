533bba7fafc2c831375d2d4681ccc5ceb6360967
tawon2137 <ziezonn@naver.com>
2016-12-10 19:16:24 +0900

    asd
    asd

--- app.js
+++
@@ -1,7 +0,0 @@
-﻿var connect = require('connect'),
-    serveStatic = require('serve-static');
-
-var app = connect();
-
-app.use(serveStatic("./"));
-app.listen(5000, function(){console.log("server on ! ")});
